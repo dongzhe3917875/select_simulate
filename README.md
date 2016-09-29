86c606c4c107ca201c6247ef6d35b4e6773cd912
dongzhe <dongzhe@360.cn>
2016-09-29 15:25:18 +0800

    add jquery for select_simulate

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+### how to use
+```
+<div class="imitate_select name-select">
+  <div class="show_select">
+    <span class="show_item">文件后缀文件后缀</span>
+    <span class="caret-wrap"></span>
+  </div>
+  <ul class="select_all hide_select">
+    <li class="select_item">目录</li>
+    <li class="select_item">文件后缀文件后缀</li>
+    <li class="select_item">精确匹配</li>
+  </ul>
+</div>
+```
+调用
+
+```
+require("select_simulate.css");
+require("./jquery.select.js");
+var select_map = new WeakMap()
+$(".imitate_select").select({
+  select_map: select_map
+})
+```
+
+
 [在线demo](http://45.62.108.67/project/select_simulate)
---
+++ jquery.select.js
@@ -0,0 +1,114 @@
+require("./jquery.changeClass.js");
+$.fn.select = function(options) {
+  var default_option = {
+    select_map: new WeakMap()
+  };
+  var options = $.extend(true, {}, default_option, options);
+  var select_map = options.select_map
+    // 根据节点计算高度
+  function heightSlide(node, status) {
+    var height = 0;
+    if (status) {
+      Array.prototype.slice.call(node.childNodes).forEach(function(child) {
+        if (child.nodeType === 1) {
+          var oStyle = window.getComputedStyle(child);
+          height = height + child.clientHeight + (parseInt(
+              oStyle.borderTopWidth) ||
+            0) + (parseInt(oStyle.borderBottomWidth) || 0);
+        }
+      });
+      height = height - parseInt(1) + "px";
+    }
+    node.style.height = height;
+  }
+  var oManager = {
+    setMapBindClass: function(node, obj) {
+      var status = !select_map.has(node);
+      if (status) {
+        select_map.set(node, obj)
+        $(node).changeClass(this.getNodeofMap(node));
+      }
+      return status;
+    },
+    getNodeofMap: function(node) {
+      return select_map.get(node);
+    },
+    getStatus: function(node, className) {
+      return select_map.get(node)[className];
+    },
+    setStatus: function(node, className, bool) {
+      select_map.get(node)[className] = bool;
+    },
+    reverseStatus: function(node, className) {
+      this.getNodeofMap(node)[className] = !this.getNodeofMap(node)[
+        className]
+    },
+    getDomArray: function(nodes) {
+      return Array.prototype.slice.call(nodes);
+    },
+    slideUp: function(node) {
+      var show_select_node = node.previousSibling;
+      if (select_map.has(node)) {
+        this.setStatus(node, "hide_select", true);
+      }
+
+      if (select_map.has(show_select_node)) {
+        this.setStatus(show_select_node, "noborder", false);
+      }
+
+      heightSlide(node, false);
+    }
+  }
+  return this.each(function() {
+    var $this = $(this);
+    var show_select = $this.find(".show_select");
+    var select_all = $this.find(".select_all");
+    var select_all_nodes = document.querySelectorAll(".select_all");
+    var select_item = ".select_item"
+    var imitate_select = $this;
+
+    show_select.on("click", function(event) {
+      event.stopPropagation();
+      var target = this;
+      oManager.setMapBindClass(target, {
+        "noborder": false
+      })
+      var select_all_node = target.nextSibling;
+      if (!oManager.setMapBindClass(select_all_node, {
+          "hide_select": false
+        })) {
+        oManager.reverseStatus(select_all_node, "hide_select");
+      }
+
+      heightSlide(select_all_node, !oManager.getStatus(
+        select_all_node, "hide_select"))
+
+      oManager.reverseStatus(target, "noborder");
+
+      var select_all_array = oManager.getDomArray(select_all_nodes);
+      select_all_array.forEach(function(ele) {
+        if (ele != select_all_node) {
+          oManager.slideUp(ele);
+        }
+      })
+    })
+
+    $(document).on("click", function(event) {
+      // 将所有的select all 收起来
+      var select_all_array = oManager.getDomArray(select_all_nodes);
+      select_all_array.forEach(function(ele) {
+        oManager.slideUp(ele);
+      })
+    })
+
+    $(document).on("click", select_item, function(event) {
+      event.stopPropagation();
+      var select_value = $(event.target).text();
+      var show_item = $(this).parents(".imitate_select").find(
+        ".show_item");
+      var select_all_node = this.parentNode;
+      show_item.text(select_value);
+      oManager.slideUp(select_all_node);
+    })
+  })
+}
